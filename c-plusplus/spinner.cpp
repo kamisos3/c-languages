6dd167fe4ed58916aa11d0ed457165207ee9d245
kamisos3 <kamilasostre11@gmail.com>
2025-11-30 21:49:19 -0400

    Practicing a simple loading animation

---
+++ c-plusplus/spinner.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <unistd.h> // To use usleep
+
+int main() {
+    std::string spinner [] = {"|", "/", "-", "\\"}
+
+    // Loops twenty times
+    for (int i = 0; i < 20; i++) {
+        std::cout << "\rLoading " << spinner[i % 4];
+        std::cout.flush(); // Forces output
+
+    // Pauses for miliseconds (200), like using async 
+        usleep(200000)
+    }
+
+    std::cout << "\rDone!  " << std::endl;
+    return 0;
+}
