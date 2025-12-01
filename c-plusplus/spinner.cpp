166e462b287ac195f5082b0bc7fdda391889e76a
kamisos3 <kamilasostre11@gmail.com>
2025-11-30 21:51:36 -0400

    Fixed syntax, file simple animation runs

--- c-plusplus/spinner.cpp
+++ c-plusplus/spinner.cpp
@@ -4,7 +4,7 @@
 #include <unistd.h> // To use usleep
 
 int main() {
-    std::string spinner [] = {"|", "/", "-", "\\"}
+    std::string spinner [] = {"|", "/", "-", "\\"};
 
     // Loops twenty times
     for (int i = 0; i < 20; i++) {
@@ -12,7 +12,7 @@ int main() {
         std::cout.flush(); // Forces output
 
     // Pauses for miliseconds (200), like using async 
-        usleep(200000)
+        usleep(200000);
     }
 
     std::cout << "\rDone!  " << std::endl;
